2554ce57b25c2c67c4abdc155d51e3eada68f37d
Matheus Vinicius <57428641+matheus778@users.noreply.github.com>
2023-01-07 17:17:18 -0300

    ajustes no mapa

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import L from 'leaflet';
 import './style.css';
-import { MapContainer, Marker, TileLayer, Popup} from 'react-leaflet'
-
+import { MapContainer, Marker, TileLayer, Popup, useMap} from 'react-leaflet';
 
 function Map() {
+  const [position, setPosition ] = useState([51.505,-0.09]);
+
+  useEffect(() => {
+    const map = L.map('map').setView(position, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: 
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map)
+
+    L.marker(position).addTo(map)
+
+    return () => {
+      map.remove();
+    }
+  },[])
+
   return (
-    
    <div id="map">
-     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
+  
    </div>
     
   )
